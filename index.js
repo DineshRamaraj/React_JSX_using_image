1598f2344584d37a54d80461b69561097fd55773
DineshRamaraj <dineshkumar26032001@gmail.com>
2023-12-04 09:43:13 +0000

    react pro using image

---
+++ index.js
@@ -0,0 +1,35 @@
+const element = (
+  <div className="congrats-card-container">
+    <h1 className="heading">Congratulations</h1>
+    <div className="card">
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/congrats-card-profile-img.png"
+        className="profile-image"
+      />
+      <h1 className="card-title">Kiran V</h1>
+      <p className="card-description">
+        Vishnu Institute of Computer Education and Technology, Bhimavaram
+      </p>
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/congrats-card-watch-img.png"
+        className="watch-image"
+      />
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
+
+// const element = (
+//   <div className="bg-container">
+//     <h1 className="heading">Congratulations</h1>
+//     <div className="card-container">
+//       <img src="https://assets.ccbp.in/frontend/react-js/congrats-card-profile-img.png" />
+//       <h1>Kiran V</h1>
+//       <p>Vishnu institute of computer Education and Technology, Bhimavaram</p>
+//       <img src="https://assets.ccbp.in/frontend/react-js/congrats-card-watch-img.png" />
+//     </div>
+//   </div>
+// );
+
+// ReactDOM.render(element, document.getElementById("root"));
